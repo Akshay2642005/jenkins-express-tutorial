b25936b81a782218cf93a1ca4c051224d1b10640
Akshay B <akshay2642005@gmail.com>
2025-04-25 12:07:03 +0530

    lazy-sidewalk

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = 5050;
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Hello World from Jenkins + Docker!');
+});
+
+
+app.get('/api/v1/status', (_req, res) => {
+  res.send('Ok');
 });
 
 app.listen(port, () => {
